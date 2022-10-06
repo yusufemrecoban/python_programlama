ca150dbe1a371fbd69d914b0f7e1ea7acac9e875
yusuf <yusuffemreecobann@gmail.com>
2022-10-06 18:28:06 +0300

    ilk proje

---
+++ hafta1/5_acıklama_yorum_satırı.py
@@ -0,0 +1,20 @@
+"""
+metin şeklinde yapılan
+yorum satırlarında kullanılır.
+"""
+
+'''
+çift tırnak veya 
+tek tırnak kullanımı yorum 
+satırları aynı etkiyi yapar.
+'''
+
+# tek satırlık yorum satırları için kullanılır.
+
+ben="""burdan 
+bütün 
+arkadaşlara 
+selam 
+olsun 
+"""
+print(ben)
