b5c16e5f0a87141a1709a406138f04c3d5852fee
yusuf <yusuffemreecobann@gmail.com>
2022-10-06 18:21:27 +0300

    ilk proje

---
+++ hafta1/4_degisken_tipleri.py
@@ -0,0 +1,27 @@
+a=1
+b=None
+c=3.5
+d=-89855
+e=True
+f="iyi"
+g='güzel'
+
+print("a :", type(a))
+print("b :", type(b))
+print("c :", type(c))
+print("d :", type(d))
+print("e :", type(e))
+print("f :", type(f))
+print("g :", type(g))
+
+
+ad=1
+soyad=2
+#önceden verilen değişken olmadan "dict" yapısı yapılmıyormuş
+x=[2,1]
+y={ ad : "yusuf", soyad :"coban"}
+z=(3,2,1)
+
+print("x :", type(x))
+print("y :", type(y))
+print("z :", type(z))
