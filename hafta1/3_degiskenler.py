4a1d276dc338461863f420781299bbd89119f578
yusuf <yusuffemreecobann@gmail.com>
2022-10-06 18:09:40 +0300

    ilk proje

---
+++ hafta1/3_degiskenler.py
@@ -0,0 +1,19 @@
+a=1
+b=None
+c=3.5
+d=-89855
+print(d)
+e=True
+print(e)
+print(a, b, c, d, e)
+print(a, b, c, d, e, sep=",")
+print(a,b,end="")
+
+print(" (hala burdayııımmmm)")
+print(e); print(c); print(a)
+b,c,d=1,1,2
+print(b,c,d)
+x="bu"
+y="ders"
+z="güzel"
+print(x,y,z)
