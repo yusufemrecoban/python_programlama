d82567e74e0e2266ddb4b337302e170fcacf9f60
yusuf <yusuffemreecobann@gmail.com>
2022-10-08 11:26:32 +0300

    ilk proje

---
+++ hafta1/12_metin_karsilastirma.py
@@ -0,0 +1,11 @@
+x="Filitre kahve"
+if x=="3 u 1 arada":
+   print("kahveye eşit")
+if x=="Filitre kahve":
+   print("kahveye eşit")
+if "kahve" in x:
+    print(" kahve var")
+if "Kahve" in x:
+    print("Kahve var")
+if "ilt" in x:
+    print("iltvar")
